8eea991313bd4abe4833e13213d99211bdb54906
Pooja13E <epooja413@gmail.com>
2025-07-08 17:06:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 5000
 connectDB()
 
 const app = express()
+const cors = require("cors")
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -20,7 +22,7 @@ app.use("/api/tickets", require("./routes/ticketRoutes"))
 
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to the Support Desk API" })
+  res.status(200).json({ message: "Welcome to the Support Desk API" })
 })
 
 app.use(errorHandler)
